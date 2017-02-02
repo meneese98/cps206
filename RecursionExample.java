1a87984a8ee7d41ebb7dcacd3a41816bfe5291c6
meneese98 <m.meneese4@gmail.com>
2017-02-02 08:51:40 -0600

    Add files via upload

---
+++ RecursionExample.java
@@ -0,0 +1,13 @@
+public class RecursionExample
+{
+    public static void main(String[] args)
+    {
+        RecursionExample recursion = new RecursionExample();
+        System.out.println(recursion.doFactorial(5));
+    }
+
+    public int doFactorial(int n)
+    {
+        //Factorial
+    }
+}
